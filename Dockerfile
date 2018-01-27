cd4095d912d17a593e9df8e8cc355eb4bb72a109
Alec Merdler <alec.merdler@coreos.com>
2018-01-26 21:40:03 -0500

    added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM clojure:alpine
+
+COPY . /usr/src/app
+WORKDIR /usr/src/app
+
+CMD ["lein", "run"]
--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [liberator "0.15.1"]
+                 [compojure "1.6.0"]
+                 [ring/ring-core "1.6.3"]
+                 [org.clojure/core.typed "0.3.21"]]
+  :plugins [[lein-typed "0.4.2"]
+            [lein-ring "0.12.2"]]
+  :injections [(require 'clojure.core.typed)
+               (clojure.core.typed/install)])
